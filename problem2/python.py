d16007c3304fbe62355b45cb929c24bcf05d2622
lian <lian.wired@gmail.com>
2017-10-12 09:58:38 +0900

    問2の解法を追加

--- problem2/python.py
+++ problem2/python.py
@@ -18,3 +18,12 @@ while True:
         result += next
     i += 1
 print(result)
+
+### Answer 2
+fib2 = [1,2]
+for i in range(100):
+    if i >= 4000000:
+        break
+    fib2.append(a[i+1] + a[i])
+
+print(sum([i for i in fib2 if (i % 2 == 0 and i < 4000000)])
